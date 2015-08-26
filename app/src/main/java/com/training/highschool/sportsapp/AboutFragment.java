4b171ce4496b201d20f3dae04410cb4df68dd9ff
Marius Bob <marius.bob@fortech.ro>
2015-08-26 12:04:32 +0300

    added custom dialog

--- app/src/main/java/com/training/highschool/sportsapp/AboutFragment.java
+++ app/src/main/java/com/training/highschool/sportsapp/AboutFragment.java
@@ -1,29 +1,35 @@
 package com.training.highschool.sportsapp;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class AboutFragment extends Fragment {
 
     private Button mShowDialog;
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_about, container, false);
 
+        Context context = this.getActivity().getApplicationContext();
+        final Dialog dialog = new Dialog(context, R.style.CustomDialogTheme);
+
         mShowDialog = (Button) rootView.findViewById(R.id.btnShowDialog);
+
         mShowDialog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getActivity().getApplicationContext(), "button clicked!", Toast.LENGTH_SHORT).show();
-                //TODO display custom dialog
+                dialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
+                dialog.setContentView(R.layout.modal_dialog);
+                dialog.show();
             }
         });
 
