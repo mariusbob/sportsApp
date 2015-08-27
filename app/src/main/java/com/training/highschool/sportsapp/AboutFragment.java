5574d24931d89f84585d1319a5ff73bc77c9c1c3
Alex Rancea <ranceaaa@gmail.com>
2015-08-27 10:05:58 +0300

    facebook

--- app/src/main/java/com/training/highschool/sportsapp/AboutFragment.java
+++ app/src/main/java/com/training/highschool/sportsapp/AboutFragment.java
@@ -9,29 +9,39 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.Toast;
+import com.facebook.FacebookSdk;
+import com.facebook.login.widget.LoginButton;
 
 public class AboutFragment extends Fragment {
 
-    private Button mShowDialog;
+    private Button mShowDialogButton;
+    LoginButton loginButton;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View rootView = inflater.inflate(R.layout.fragment_about, container, false);
+        final View rootView = inflater.inflate(R.layout.fragment_about, container, false);
+        FacebookSdk.sdkInitialize(getActivity().getApplicationContext());
+
+
 
         Context context = this.getActivity().getApplicationContext();
         final Dialog dialog = new Dialog(context, R.style.CustomDialogTheme);
 
-        mShowDialog = (Button) rootView.findViewById(R.id.btnShowDialog);
+        mShowDialogButton = (Button) rootView.findViewById(R.id.btnShowDialog);
 
-        mShowDialog.setOnClickListener(new View.OnClickListener() {
+        mShowDialogButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 dialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
                 dialog.setContentView(R.layout.modal_dialog);
                 dialog.show();
+
+
             }
         });
+        loginButton = (LoginButton) dialog.findViewById(R.id.login_button);
+
 
         return rootView;
     }
