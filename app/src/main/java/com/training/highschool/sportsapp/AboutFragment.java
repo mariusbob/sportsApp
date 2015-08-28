c62f3bb481991e75fb045647b12b9191d92883ef
Marius Bob <marius.bob@fortech.ro>
2015-08-28 09:49:50 +0300

    changed Dialog to DialogFragment

--- app/src/main/java/com/training/highschool/sportsapp/AboutFragment.java
+++ app/src/main/java/com/training/highschool/sportsapp/AboutFragment.java
@@ -1,48 +1,40 @@
 package com.training.highschool.sportsapp;
 
-import android.app.Dialog;
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.widget.Button;
-import com.facebook.FacebookSdk;
-import com.facebook.login.widget.LoginButton;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class AboutFragment extends Fragment {
 
     private Button mShowDialogButton;
-    LoginButton loginButton;
+    TextView tvTitle;
 
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         final View rootView = inflater.inflate(R.layout.fragment_about, container, false);
-        FacebookSdk.sdkInitialize(getActivity().getApplicationContext());
+        return rootView;
+    }
 
-
-
-        Context context = this.getActivity().getApplicationContext();
-        final Dialog dialog = new Dialog(context, R.style.CustomDialogTheme);
-
-        mShowDialogButton = (Button) rootView.findViewById(R.id.btnShowDialog);
-
+    @Override
+    public void onViewCreated(View view, Bundle savedInstance){
+        tvTitle = (TextView) view.findViewById(R.id.tvTitle);
+        mShowDialogButton = (Button) view.findViewById(R.id.btnShowDialog);
         mShowDialogButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                dialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
-                dialog.setContentView(R.layout.modal_dialog);
-                dialog.show();
-
-
+                DialFragment df = new DialFragment();
+                android.support.v4.app.FragmentManager fm = getActivity().getSupportFragmentManager();
+                df.show(fm, "");
             }
         });
-        loginButton = (LoginButton) dialog.findViewById(R.id.login_button);
-
-
-        return rootView;
     }
+
 }
---
+++ app/src/main/java/com/training/highschool/sportsapp/DialFragment.java
@@ -0,0 +1,74 @@
+package com.training.highschool.sportsapp;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class DialFragment extends DialogFragment {
+
+    LoginButton loginButton;
+    CallbackManager callbackManager;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        FacebookSdk.sdkInitialize(getActivity());
+        callbackManager = CallbackManager.Factory.create();
+        getDialog().getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        View rootView = inflater.inflate(R.layout.modal_dialog, container, false);
+        loginButton = (LoginButton) rootView.findViewById(R.id.login_button);
+        loginButton.setFragment(this);
+        loginButton.setReadPermissions("public_profile");
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject jsonObject, GraphResponse graphResponse) {
+                        String email = jsonObject.optString("name");
+
+                        Log.d("USERU ", email);
+
+                    }
+                }).executeAsync();
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+
+            }
+        });
+
+        return rootView;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+}
